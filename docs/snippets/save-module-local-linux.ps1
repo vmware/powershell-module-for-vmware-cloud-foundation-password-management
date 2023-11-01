5f448c7a8a36eea30c00f4b120ade893fb9050cf
Jared Burns <burnsja@vmware.com>
2023-11-01 15:03:58 -0400

    docs: add air-gapped install
    Adding documentation to install modules and dependencies in a air-gapped environment.
    
    Signed-off-by: Jared Burns <burnsja@vmware.com>

---
+++ docs/snippets/save-module-local-linux.ps1
@@ -0,0 +1,5 @@
+Save-Module -Name VMware.vSphere.SsoAdmin -MinimumVersion 1.3.9  -Path /home/modules
+Save-Module -Name VMware.PowerCLI -MinimumVersion 13.0.0 -Path /home/modules
+Save-Module -Name PowerVCF -MinimumVersion 2.4.0 -Path /home/modules
+Save-Module -Name PowerValidatedSolutions -MinimumVersion 2.6.0 -Path /home/modules
+Save-Module -Name VMware.CloudFoundation.PasswordManagement -Path /home/modules
